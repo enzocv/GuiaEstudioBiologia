7c08f56cc19138d3e497d2b405bc4cfa62ac5782
Enzo Catalán Vargas <enzocv96@gmail.com>
2017-05-27 21:32:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/enzo/guiaestudiobiologia/GuiaBiologiaMain.java
@@ -0,0 +1,13 @@
+package com.example.enzo.guiaestudiobiologia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuiaBiologiaMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guia_biologia_main);
+    }
+}
